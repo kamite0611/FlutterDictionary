0b256e95cc56c13590b0f12275aeae8211d43b2b
kamite0611 <kamiteakira@gmail.com>
2023-10-23 17:43:23 +0900

    fix locale domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,17 +4,6 @@ const nextConfig = {
   i18n: {
     locales: ['en', 'ja'],
     defaultLocale: 'en',
-
-    domains: [
-      {
-        domain: 'flutter-gallery.com',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'ja.flutter-gallery.com',
-        defaultLocale: 'ja',
-      },
-    ],
   },
   experimental: {
     scrollRestoration: true,
